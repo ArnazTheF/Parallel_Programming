e1d60a0ec93a04cfbd434ab6e740bae00a0c1a18
ArnazTheF <semarsdmi@gmail.com>
2025-02-14 05:09:54 +0000

    task1

---
+++ task1_makefile/main.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES
+#define DOUBLE
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+template<typename T>
+void calculateAndPrintSum() {
+    T pi = static_cast<T>(M_PI);
+    std::vector<T> divided(1e7, (2 * pi) / 1e7);
+
+    T sum = 0;
+    T counter = 0;
+    for (T part : divided) {
+        sum += std::sin(part * counter);
+        counter++;
+    }
+
+    std::cout << sum << std::endl;
+}
+
+int main() {
+    #ifdef FLOAT
+        calculateAndPrintSum<float>();
+    #endif
+    #ifdef DOUBLE
+        calculateAndPrintSum<double>();
+    #endif
+
+    return 0;
+}
